2f3bc1c4f47045d334d42b2a54e38a2ccb723639
Marcos Leon Borges Filho <marcosleon200412@gmail.com>
2023-11-30 16:11:54 -0300

    Editing files

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,21 +1,57 @@
 import{Request, Response} from 'express';
+import {Pet} from '../models/pet';
+import {creaMenuObject} from '../helpers/createMenuObject';
+import { type } from 'os';
+
 
 export const home = (req: Request, res: Response)=>{
+        let list = Pet.getAll();
+        
+    res.render('pages/page.mustache', {
+        menu: creaMenuObject('all'),
 
- 
+        banner:{
+            title: 'Todos os animais',
+            background:'allanimals.jpg'
+            
+        }
+    }),
+    list
 }
 
 export const dogs = (req: Request, res: Response)=>{
 
+    res.render('pages/page.mustache', {
+        menu: creaMenuObject('dog'),
 
+        banner:{
+            title: 'Cachorros',
+            background:'banner_dog.jpg'
+        }
+    })
 }
 
 export const cats = (req: Request, res: Response)=>{
 
-
+    res.render('pages/page.mustache', {
+        menu: creaMenuObject('cat'),
+           
+        banner:{
+            title: 'Gatos',
+            background:'banner_cat.jpg'
+        }
+    })
 }
 
 export const fishes = (req: Request, res: Response)=>{
 
+    res.render('pages/page.mustache', {
 
+        menu: creaMenuObject('fish'),
+
+        banner:{
+            title: 'Peixes',
+            background:'banner_fish.jpg'
+        }
+    })
 }
---
+++ src/models/pet.ts
@@ -0,0 +1,154 @@
+type PetType = 'dog' | 'cat' | 'fish'
+
+type Pet = {
+    type: 'dog' | 'cat' | 'fish',
+    image: string,
+    name: string,
+    color: string,
+    sex: 'Masculino' | 'Feminino'
+}
+
+const data: Pet[]= [
+    {
+        type: 'dog',
+        image: 'images/pastor-alemao.jpg',
+        name: 'Pastor-alemão',
+        color: 'Amarelo e Preto',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'images/labrador.jpg',
+        name: 'Labrador-retriever',
+        color: 'Branco',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'zwergspitz.jpg',
+        name: 'Zwergspitz',
+        color: 'Amarelo',
+        sex: 'Feminino'
+    },
+    {
+        type: 'dog',
+        image: 'images/husky.jpg',
+        name: 'Husky Siberiano',
+        color: 'Branco e Preto',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'images/golden.jpg',
+        name: 'Golden Retriever',
+        color: 'Amarelo',
+        sex: 'Masculino'
+    },
+    {
+        type: 'dog',
+        image: 'images/poodle.jpg',
+        name: 'Poodle',
+        color: 'Branco',
+        sex: 'Feminino'
+    },
+    {
+        type: 'dog',
+        image: 'images/bulldog.jpg',
+        name: 'Bulldog',
+        color: 'Branco e Amarelo',
+        sex: 'Masculino'
+    },
+    {
+        type: 'cat',
+        image: 'images/persa.jpg',
+        name: 'Persa',
+        color: 'Amarelo',
+        sex: 'Masculino'
+    },
+    {
+        type: 'cat',
+        image: 'images/mainecoon.jpg',
+        name: 'Maine Coon',
+        color: 'Preto e Branco',
+        sex: 'Masculino'
+    },
+    {
+        type: 'cat',
+        image: 'images/bengal.jpg',
+        name: 'Bengal',
+        color: 'Branco, Preto e Amarelo',
+        sex: 'Feminino'
+    },
+    {
+        type: 'cat',
+        image: 'images/siames.jpg',
+        name: 'Siamês',
+        color: 'Amarelo e Preto',
+        sex: 'Masculino'
+    },
+    {
+        type: 'cat',
+        image: 'images/sphynx.jpg',
+        name: 'Sphynx',
+        color: 'Branco',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'images/neon.jpg',
+        name: 'Tetra Neon',
+        color: 'Vermelho e Azul',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'images/matogrosso.jpg',
+        name: 'Mato Grosso',
+        color: 'Laranja',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'images/limpavidro.jpg',
+        name: 'Limpa Vidro',
+        color: 'Verde e Branco',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'images/tanictis.jpg',
+        name: 'Tanictis',
+        color: 'Vermelho',
+        sex: 'Masculino'
+    },
+    {
+        type: 'fish',
+        image: 'images/acara.jpg',
+        name: 'Acará Bandeira',
+        color: 'Preto',
+        sex: 'Masculino'
+    },
+]
+
+
+export const Pet = {
+    getAll: (): Pet[] =>{
+        return data;
+    },
+
+    getFromType: (type: PetType): Pet[] => {
+        return data.filter(item => item.type === type);
+    },
+
+    getFromName: (name: string): Pet[] =>{
+        return data.filter(item=>{
+            if(item.name.toLowerCase().indexOf(name.toLowerCase()) > -1){
+                return true;
+            } else {
+                return false;
+            }
+        })
+    }
+}
+
+
